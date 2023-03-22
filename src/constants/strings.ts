95afabe825e55ebab2872f2d40496e59b9e3f625
Nitzan Papini <nitzanpap@gmail.com>
2023-03-22 12:27:36 +0200

    Added some text descriptions

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -2,10 +2,27 @@ const DESIGN_SYSTEM_GENERATED = 'Design system generated!'
 const NO_SELECTION = 'You need to select figma elements first!'
 const NO_ELEMENTS_FOUND = 'No elements were found to generate the Design System...'
 const ELEMENTS_SELECTED = 'Elements selected!'
+const APP_PRIMARY_FONT_NAME: FontName = {
+    family: 'Roboto',
+    style: 'Bold',
+}
+const APP_SECONDARY_FONT_NAME: FontName = {
+    family: 'Roboto',
+    style: 'Medium',
+}
+const APP_REGULAR_FONT_NAME: FontName = {
+    family: 'Roboto',
+    style: 'Regular',
+}
+const DESCRIPTION_TEXT_GAP = 24
 
 export {
     DESIGN_SYSTEM_GENERATED,
     NO_SELECTION,
     NO_ELEMENTS_FOUND,
-    ELEMENTS_SELECTED
+    ELEMENTS_SELECTED,
+    APP_PRIMARY_FONT_NAME,
+    APP_SECONDARY_FONT_NAME,
+    APP_REGULAR_FONT_NAME,
+    DESCRIPTION_TEXT_GAP,
 }
--- src/plugin/controller.ts
+++ src/plugin/controller.ts
@@ -3,6 +3,7 @@ import { MsgTypes } from '../enums/MsgTypes.enum'
 import { colorsUtils } from './services/colors.service'
 import { msgsUtils } from './services/msgs.utils'
 import { fontsUtils } from './services/fonts.service'
+import { APP_PRIMARY_FONT_NAME } from '../constants/strings'
 
 figma.showUI(__html__)
 
@@ -36,6 +37,7 @@ figma.ui.onmessage = (msg) => {
 async function generateDesignSystem() {
     console.log('Generating Design System...')
 
+    await figma.loadFontAsync(APP_PRIMARY_FONT_NAME)
 
     if (!selection.length)
         return msgsUtils.postMsg(MsgTypes.NO_SELECTION, Strings.NO_SELECTION)
@@ -66,10 +68,10 @@ async function generateDesignSystem() {
             else if (type === 'TEXT' && node.fontName) {
                 // If the fontName is not mixed, add it to the array
                 if (node.fontName !== figma.mixed && node.fontSize !== figma.mixed) {
-                    const appTextNode: AppTextNode = {
+                    const appTextNode: AppFontNode = {
                         fontName: node.fontName,
-                        fontFamily: node.fontName.family,
-                        fontStyle: node.fontName.style,
+                        family: node.fontName.family,
+                        style: node.fontName.style,
                         fontSize: node.fontSize,
                     }
                     uniqueFonts.add(JSON.stringify(appTextNode))
